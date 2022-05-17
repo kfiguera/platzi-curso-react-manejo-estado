25ca2796557665c9d8200c6c1440193dc9287e37
Kevin Figuera <kevinalejandro3@gmail.com>
2022-05-17 11:50:20 -0400

    Estados independientes con useState

--- src/UseState.js
+++ src/UseState.js
@@ -1,20 +1,21 @@
 import React from "react";
-
+const SECURITY_CODE = "andrea";
 function UseState({name}) {
+    const [value, setValue] = React.useState('');
     const [error, setError] = React.useState(false);
     const [loading, setLoading] = React.useState(false);
 
     React.useEffect(() => {
-        console.log("UseState: useEffect");
+
         if (!!loading) {
+            setError(false);
             setTimeout(() => {
-                console.log("UseState: setTimeout");
+                if (value !== SECURITY_CODE) {
+                    setError(true);
+                }
                 setLoading(false);
-                setError(!error);
-                console.log("UseState: setTimeout end");
             }, 3000);
         }
-        console.log("UseState: useEffect end");
     }, [loading]);
 
     return (
@@ -22,12 +23,18 @@ function UseState({name}) {
             <h2>Eliminar {name}</h2>
             <p>Por favor, escribe el código de seguridad</p>
             {error && (
-                <p> El Código incorrecto</p>
+                <p> El Código es incorrecto</p>
             )}
             {loading && (
                 <p> Cargando...</p>
             )}
-            <input placeholder="Código de Seguridad"/>
+            <input
+                value={value}
+                onChange={(event) => {
+                    setValue(event.target.value)
+                }}
+                placeholder="Código de Seguridad"
+            />
             <button
                 onClick={() => setLoading(!loading)}
             >Comprobar
