60aab0ce97fa6cd46cc44d08b8ddc6000923e91f
Kevin Figuera <kevinalejandro3@gmail.com>
2022-05-17 19:58:11 -0400

    Estados compuestos con useState

--- src/UseState.js
+++ src/UseState.js
@@ -1,42 +1,66 @@
 import React from "react";
+
 const SECURITY_CODE = "andrea";
+
 function UseState({name}) {
+    const [state, setState] = React.useState({
+        value: "",
+        error: false,
+        loading: false
+    });
+
     const [value, setValue] = React.useState('');
     const [error, setError] = React.useState(false);
     const [loading, setLoading] = React.useState(false);
 
     React.useEffect(() => {
 
-        if (!!loading) {
-            setError(false);
+        if (!!state.loading) {
+            setState({
+                ...state, error: false
+            });
             setTimeout(() => {
-                if (value !== SECURITY_CODE) {
-                    setError(true);
+                if (state.value !== SECURITY_CODE) {
+                    setState({
+                        ...state, error: true, loading: false
+                    });
+                }else{
+                    setState({
+                        ...state, error: false, loading: false
+                    });
                 }
-                setLoading(false);
+
             }, 3000);
         }
-    }, [loading]);
+    }, [state]);
 
     return (
         <div>
             <h2>Eliminar {name}</h2>
             <p>Por favor, escribe el código de seguridad</p>
-            { (!loading && error) && (
+            {(!state.loading && state.error) && (
                 <p> El Código es incorrecto</p>
             )}
-            {loading && (
+            {state.loading && (
                 <p> Cargando...</p>
             )}
             <input
-                value={value}
+                value={state.value}
                 onChange={(event) => {
-                    setValue(event.target.value)
+                    setState({
+                        ...state,
+                        value: event.target.value
+                    });
                 }}
                 placeholder="Código de Seguridad"
             />
             <button
-                onClick={() => setLoading(!loading)}
+                onClick={() => {
+                    setState({
+                        ...state,
+                        loading: true
+                    })
+                }}
             >Comprobar
             </button>
         </div>
